7db3aa8b44e682e43623cd65e4c05043f8589050
Sh4rkzDev <lucasgarciacampos@gmail.com>
2024-07-10 22:24:31 -0300

    Exercise 3 done

---
+++ exercise3/commands.go
@@ -0,0 +1,23 @@
+package main
+
+type Command func()
+
+func Right() {
+	return
+}
+
+func Left() {
+	return
+}
+
+func Pick() {
+	return
+}
+
+func Place() {
+	return
+}
+
+func Warning() {
+	return
+}
---
+++ exercise3/robot.go
@@ -0,0 +1,39 @@
+package main
+
+// Returns the command to be executed
+func Solve(clawPos int, boxes []int, boxInClaw int) Command {
+	min, max := getMinMaxStacks(boxes)
+	dir := 0
+	if boxInClaw != 0 {
+		dir = clawPos - min
+		if dir == 0 {
+			if boxes[clawPos] == 5 { // This condition can be verified before, but I am following the exercise statement
+				return Warning
+			}
+			return Place
+		}
+	} else {
+		dir = clawPos - max
+		if dir == 0 {
+			return Pick
+		}
+	}
+	if dir > 0 {
+		return Left
+	} else {
+		return Right
+	}
+}
+
+// Returns the index of the stack with least boxes and the index of the stack with most boxes.
+func getMinMaxStacks(boxes []int) (int, int) {
+	min, max := 0, 0
+	for idx, box := range boxes {
+		if box < boxes[min] {
+			min = idx
+		} else if box >= boxes[max] {
+			max = idx
+		}
+	}
+	return min, max
+}
